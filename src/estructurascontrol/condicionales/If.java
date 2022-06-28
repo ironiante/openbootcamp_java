0dabbdf7a54c0bb31af0bf9bd9337defb718247d
jirman rodriguez <87489149+ironiante@users.noreply.github.com>
2022-06-27 21:23:47 -0500

    seleccion simple if

--- src/estructurascontrol/condicionales/If.java
+++ src/estructurascontrol/condicionales/If.java
@@ -1,4 +1,23 @@
 package estructurascontrol.condicionales;
 
+/**
+ * seleccion simple,
+ */
 public class If {
+    public static void main(String[] args) {
+/**
+ * si la calificacion del estudiante es mayor o igual que 60.
+ *   imprimir "aprobado"
+  */
+  calificacionEstudiante();
+    }
+
+    private static void calificacionEstudiante() {
+        double calificacion = 61;
+        if (calificacion >=60){
+            System.out.println("aprobado");
+        }
+    }
+
+
 }
