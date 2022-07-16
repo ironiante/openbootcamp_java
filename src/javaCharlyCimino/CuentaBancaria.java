7fd50df93d7dc014989a4a5c774518fcb77a5358
jirman rodriguez <87489149+ironiante@users.noreply.github.com>
2022-07-16 13:01:03 -0500

    crear objetos

--- src/javaCharlyCimino/CuentaBancaria.java
+++ src/javaCharlyCimino/CuentaBancaria.java
@@ -4,6 +4,10 @@ package javaCharlyCimino;
  * 1. comportamiento de un objeto: metodos en java.
  * 2. creamos un objeto cuenta bancaria, en la clase principal.
  * 3. los objetos tienen comportamientos , creamos un metodo
+ * 4. creamos un metodo con retorno
+ * 5. creamos metodos con parametros ( depositar).
+ * 6. creamos un metodo que devuelva algo y que tambien reciba parametros ( saldo disponible).
+ * 7. metodo extraer
  */
 public class CuentaBancaria {
 /**
@@ -18,6 +22,27 @@ double saldo;
     void mostrarDatos(){
         System.out.println(cbu  +   " "   +  alias  +   " "   +  saldo);
     }
+//_____________________________________________________________________________________________
+ // 4. creamos un metodo con retorno
+ double obtenerSaldo (){
+ return saldo;
+ }
+ // 5. metodo depositar con parametros.
+    void depositar(double monto){
+        if (monto >0){
+            saldo = saldo + monto; // o saldo +=monto
+        }
 
+    }
+//6.  saldo disponible
+boolean saldoDisponible(double monto){
+        return saldo >= monto;
+}
+// metodo extraer
+void extraer (double monto){
+        if (saldoDisponible(monto)){
+            saldo -= monto;
+        }
+}
 
 }
--- src/javaCharlyCimino/PrincipalCuentaBancaria.java
+++ src/javaCharlyCimino/PrincipalCuentaBancaria.java
@@ -10,5 +10,20 @@ cb.cbu ="2500000000000";
 cb.alias = "mi cuenta";
 cb.mostrarDatos();
 
+double x = cb.obtenerSaldo(); // creamos una variable para guardar saldo y despues lo imprimimos.
+System.out.println(x);
+cb.depositar(200);
+        cb.mostrarDatos();
+        cb.depositar(-800);
+        cb.obtenerSaldo();
+// hacemos la consulta y prueba del saldo disponible.
+        System.out.println(cb.saldoDisponible(100));
+        System.out.println(cb.saldoDisponible(300));
+// probamos metodo extraer
+cb.extraer(180);
+        System.out.println(cb.obtenerSaldo());
+        cb.extraer(100);
+        System.out.println(cb.obtenerSaldo());
+
     }
 }
